3fc3f6820b5f0f3b04bd5fd2a9db6728fa1c3d92
Cupico <davidelbar2@gmail.com>
2021-12-14 11:03:30 +0100

    ok

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import io from "socket.io-client";
 
-export const endpoint = "http://localhost:3000";
+export const endpoint = "https://devlab-back.herokuapp.com";
+
+// host : https://devlab-back.herokuapp.com/
 
 export const socket = io(endpoint);
 
