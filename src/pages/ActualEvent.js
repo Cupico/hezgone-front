8f8f00ddd9fdc797ba29be2fc3413416d8c30448
Esteban <45163227+Zoumbar@users.noreply.github.com>
2022-03-25 16:12:48 +0100

    Event page part 1

--- src/pages/ActualEvent.js
+++ src/pages/ActualEvent.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react";
-import { Box, Grid, Avatar, Text, Heading, Badge } from "@chakra-ui/react";
-
+import { Box, Grid, Avatar, Text, Heading, Badge, Flex, SimpleGrid, Center } from "@chakra-ui/react";
+import {BsFillGeoAltFill, BsLink, BsFillCalendarWeekFill, BsEyeFill, BsMusicNoteBeamed, BsFillWalletFill} from 'react-icons/bs'
+import { BiDrink, BiCar } from "react-icons/bi";
 import { useParams } from "react-router";
 
 import { UserContext } from "../context/User";
@@ -40,12 +41,46 @@ function ActualEvent() {
   }, [Event.eventGlobal.code, room.id]);
 
   return (
-    <Box px={"5%"}>
+    <Box>
       <Chats />
 
       {Event && Object.keys(Event.eventGlobal).length > 0 && (
-        <Box>
-          <Box p={0} display="flex" justifyContent={"space-between"} mb={14}>
+        <Box m={6}>
+          <Box p={0} display="flex" justifyContent={"space-between"} mb={5}>
+            <Box>
+              <Heading fontWeight={"bold"} fontSize={{ base: "4xl", sm: "4xl", md: "5xl" }} mt={4}>
+                      {Event.eventGlobal.name}
+              </Heading>
+              <BsFillGeoAltFill style={{display: "initial", color: "#A7DACE"}} /> Paris
+            </Box>
+            <Box height='100%' p={4} width={"100px"} style={{textAlign: "center"}}>
+              <Box fontWeight={'bold'} fontSize={'2xl'} height='100%' pt={4} pb={4} bg={"#69CEB7"} style={{textAlign: "center"}} rounded={10} >
+                <BsFillCalendarWeekFill style={{color: "#fff", display: 'initial'}} />
+                <Heading fontWeight={"500"} color={'#fff'} mt={-3}>05</Heading>
+                <Text color={'#fff'} fontSize={'md'} mt={-2}>Fev.</Text>
+              </Box>
+              <BsLink fontSize={'24px'} style={{color:'#69CEB7', display: 'initial'}}  />
+            </Box>
+          </Box>
+          <Heading fontWeight={"bold"} fontSize={{ base: "xl"}} mb={2}>
+            Invités
+          </Heading>
+          <Flex mb={5}>
+            <Box height={'50px'} width={'50px'} bg={'#69CEB7'} rounded={'50%'} style={{border: '3px solid #fff'}}></Box>
+            <Box height={'50px'} width={'50px'} bg={'#69CEB7'} rounded={'50%'} style={{border: '3px solid #fff'}} ml={-3}></Box>
+            <Box height={'50px'} width={'50px'} bg={'#69CEB7'} rounded={'50%'} style={{border: '3px solid #fff', textAlign: "center"}} ml={-3}><BsEyeFill style={{color: "#fff", display: 'initial', marginTop:'14px'}} /></Box>
+          </Flex>
+          <SimpleGrid columns={2} spacingY={'20px'} spacingX={'10px'} >
+            <Center height={'150px'} width={'150px'} boxShadow='0px 0px 10px rgba(0, 0, 0, 0.2)' rounded={'lg'}><BsMusicNoteBeamed  /></Center>
+            <Center height={'150px'} width={'150px'} boxShadow='0px 0px 10px rgba(0, 0, 0, 0.2)' rounded={'lg'}><BsFillWalletFill /></Center>
+            <Center height={'150px'} width={'150px'} boxShadow='0px 0px 10px rgba(0, 0, 0, 0.2)' rounded={'lg'}><BiDrink /></Center>
+            <Center height={'150px'} width={'150px'} boxShadow='0px 0px 10px rgba(0, 0, 0, 0.2)' rounded={'lg'}><BiCar /></Center>
+          </SimpleGrid>
+
+
+
+
+          <Box p={0} mb={14}>
             <Box
               display="flex"
               flexDirection="column"
@@ -54,14 +89,6 @@ function ActualEvent() {
               paddingY={2}
             >
               <Box display="flex" alignItems={"center"} marginBottom={8}>
-                <Heading
-                  fontWeight={600}
-                  fontSize={{ base: "2xl", sm: "4xl", md: "5xl" }}
-          
-                >
-                  {Event.eventGlobal.name}
-                </Heading>
-
                 <Badge colorScheme="purple" fontSize={"1.5rem"} marginLeft="auto" marginTop={2}>
                   {Event.eventGlobal.code}
                 </Badge>
